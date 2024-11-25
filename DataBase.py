b2f03b3ad310e80579aa95ba4d5289c0d099929e
blink <blink@>
2024-11-25 22:22:18 +0300

    update database (creating tables)

--- DataBase.py
+++ DataBase.py
@@ -1,31 +1,43 @@
 import sqlite3
+
 class DataBase:
     def __init__(self):
         pass
-    conn = sqlite3.connect('dataBase.db')
-    conn.execute('''CREATE TABLE User
-    (ID INT PRIMARY KEY NOT NULL,
-    Fname TEXT NOT NULL,
-    Lname TEXT NOT NULL,
-    Type TEXT NOT NULL,
-    Email TEXT NOT NULL,                 
-    Phone_Number INT NOT NULL,
-    Password TEXT NOT NULL);''')
-    print ("Table created successfully")
-    conn.execute('''CREATE TABLE Package
-    (ID INT PRIMARY KEY NOT NULL,
-    office_id INT NOT NULL,
-    recieverID INT NOT NULL,
-    senderID INT NOT NULL,
-    tracking_number TEXT NOT NULL,                 
-    dimenstions TEXT NOT NULL,
-    weight REAL NOT NULL,
-    FOREIGN KEY (office_id) REFERENCES User(id) ON DELETE CASCADE,
-    FOREIGN KEY (reciever_id) REFERENCES User(id) ON DELETE CASCADE             
-    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE             
-                      
-                 );''')
-    conn.close()
 
+    def initiate_database(self):
+        conn = sqlite3.connect('dataBase.db')
 
+        conn.execute('''CREATE TABLE USER
+        (ID INT PRIMARY KEY NOT NULL,
+        F_NAME TEXT NOT NULL,
+        L_NAME TEXT NOT NULL,
+        TYPE TEXT NOT NULL,
+        EMAIL TEXT NOT NULL,                 
+        PHONE_NUMBER INT NOT NULL,
+        PASSWORD TEXT NOT NULL);''')
+        print("USER Table created successfully")
 
+        conn.execute('''CREATE TABLE OFFICE
+        (ID INT PRIMARY KEY NOT NULL,        
+        NAME TEXT NOT NULL   
+        );''')
+        print("OFFICE Table created successfully")
+
+        conn.execute('''CREATE TABLE PACKAGE
+        (ID INT PRIMARY KEY NOT NULL,
+        OFFICE_ID INT NOT NULL,
+        RECEIVER_ID INT NOT NULL,
+        SENDER_ID INT NOT NULL,
+        TRACKING_NUMBER TEXT NOT NULL,                 
+        DIMENSIONS TEXT NOT NULL,
+        WEIGHT REAL NOT NULL,
+        FOREIGN KEY (OFFICE_ID) REFERENCES OFFICE(ID) ON DELETE CASCADE,
+        FOREIGN KEY (RECEIVER_ID) REFERENCES USER(ID) ON DELETE CASCADE,
+        FOREIGN KEY (SENDER_ID) REFERENCES USER(ID) ON DELETE CASCADE
+        );''')
+        print("PACKAGE Table created successfully")
+
+        conn.close()
+
+db = DataBase()
+db.initiate_database()
